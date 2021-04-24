3f0d2bf0fa84725b5cf97d89b4219f27f7ff683e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-23 21:13:13 -0600

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -16,7 +16,7 @@
     </main>
 
     <footer class="footer">
-      <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }}. </span>
+      <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }} Infinity Bot List | All Rights Reserved. </span>
     </footer>
 
   </div>
